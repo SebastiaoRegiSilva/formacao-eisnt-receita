ed6cc1883f6df82a09b8f6934bf085e368c95e00
Sebastião Willian <swillian2009@gmail.com>
2024-12-26 19:33:05 +0000

    Implementação de acesso as descrições de enums.

---
+++ formacao-eisnt-receita/Helpers/EnumHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace formacao_eisnt_receita.Helpers
+{
+	/// <summary>
+	/// Utilização as descrições de Enums.
+	/// </summary>
+	public static class EnumHelper
+	{
+		public static string GetEnumDescription(Enum value)
+		{
+			var field = value.GetType().GetField(value.ToString());
+			var attribute = field.GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault() as DescriptionAttribute;
+
+			return attribute?.Description ?? value.ToString();
+		}
+	}
+}
